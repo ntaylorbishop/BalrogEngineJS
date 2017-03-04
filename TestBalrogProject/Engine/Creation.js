cc68373b3b3509538b4a5131f68a5fa802e7f6cd
Taylor Bishop <ntaylorbishop@gmail.com>
2017-03-03 23:25:38 -0600

    First vert / frag shader

---
+++ TestBalrogProject/Engine/Creation.js
@@ -0,0 +1,72 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//VARS
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//---------------------------------------------------------------------------------------------------------------------------
+var gl;
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//INIT
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//---------------------------------------------------------------------------------------------------------------------------
+function InitWebGL(canvas) {
+    gl = null;
+
+    // Try to grab the standard context. If it fails, fallback to experimental.
+    gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+
+    // If we don't have a GL context, give up now
+    if (!gl) {
+        alert('Unable to initialize WebGL. Your browser may not support it.');
+    }
+
+    return gl;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//START
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//---------------------------------------------------------------------------------------------------------------------------
+function start() {
+    var canvas = document.getElementById('glCanvas');
+
+    // Initialize the GL context
+    gl = InitWebGL(canvas);
+
+    // Only continue if WebGL is available and working
+    if (!gl) {
+
+        return;
+    }
+
+    // Set clear color to black, fully opaque
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Enable depth testing
+    gl.enable(gl.DEPTH_TEST);
+    // Near things obscure far things
+    gl.depthFunc(gl.LEQUAL);
+    // Clear the color as well as the depth buffer.
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+
+    //COMPILE SHADER
+    var v = document.getElementById("vertex").firstChild.nodeValue;
+    var f = document.getElementById("fragment").firstChild.nodeValue;
+
+    var vs = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vs, v);
+    gl.compileShader(vs);
+
+    var fs = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fs, f);
+    gl.compileShader(fs);
+
+    program = gl.createProgram();
+    gl.attachShader(program, vs);
+    gl.attachShader(program, fs);
+    gl.linkProgram(program);
+}
