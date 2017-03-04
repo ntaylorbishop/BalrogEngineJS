20c7718f52571af92f36f399455a3f704850a583
Taylor Bishop <ntaylorbishop@gmail.com>
2017-03-04 15:25:57 -0600

    Merge branch 'master' of https://github.com/ntaylorbishop/BalrogEngineJS
    # Conflicts:
    #	TestBalrogProject/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache
    #	TestBalrogProject/obj/Debug/TestBalrogProject.csproj.FileListAbsolute.txt
    #	TestBalrogProject/obj/Debug/TestBalrogProject.csprojResolveAssemblyReference.cache
    #	TestBalrogProject/obj/Debug/TestBalrogProject.dll
    #	TestBalrogProject/obj/Debug/TestBalrogProject.pdb

---
+++ TestBalrogProject/Engine/BalrogEngine.js
@@ -0,0 +1,39 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//GLOBAL VARS
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//---------------------------------------------------------------------------------------------------------------------------
+var m_timeAtLastFrame   = new Date().getTime();
+var m_idealTimePerFrame = 1000 / 30;
+var m_leftover          = 0.0;
+var m_frames            = 0;
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//RUN
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//---------------------------------------------------------------------------------------------------------------------------
+function RunApplication() {
+
+    setInterval(TickEngine, 1000 / 30);
+}
+
+
+//---------------------------------------------------------------------------------------------------------------------------
+function TickEngine() {
+
+    var timeAtThisFrame = new Date().getTime();
+    var timeSinceLastDoLogic = (timeAtThisFrame - m_timeAtLastFrame) + leftover;
+    var catchUpFrameCount  = Math.floor(timeSinceLastDoLogic / m_idealTimePerFrame);
+
+    for (i = 0; i < catchUpFrameCount; i++) {
+        Update();
+        frames++;
+    }
+
+    renderer.renderScene();
+    
+    leftover = timeSinceLastDoLogic - (catchUpFrameCount * m_idealTimePerFrame);
+    m_timeAtLastFrame = timeAtThisFrame;
+}
